b9d7cc30b20cf80da4bcf01a755f06744b5c66c9
Erik Peteri <peteri.erik@gmail.com>
2023-11-03 18:47:18 +0200

    add material ui and style the sign up form

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,80 +1,80 @@
 import { NavLink } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message"
-
 import { registerUser, reset } from "../../actions/AuthActions";
 import styles from "../../assets/authForms.module.css";
+import { Button, TextField } from "@mui/material";
 
 const Signup = () => {
   const dispatch = useDispatch();
-  const { register, handleSubmit, watch, formState: { errors }, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     const userData = {
       username: data.username,
       password1: data.password1,
-      password2: data.password2
-    }
-    dispatch(registerUser(userData))
-    reset()
-  }
- 
+      password2: data.password2,
+    };
+    dispatch(registerUser(userData));
+    reset();
+  };
+
   return (
     <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-      <h2 className={styles.title}>Create An Account</h2>
+      <h2>Create An Account</h2>
       <div className={styles.body}>
-        <div>
-          <label className={styles.label} htmlFor="username">
-            Username{" "}
-          </label>
-          <input
-            className={styles.field}
-            type="text"
-            placeholder="username" {...register("username", { required: true })}
+        <div style={{ marginBottom: "16px" }}>
+          <TextField
+            fullWidth
+            label="Username"
+            placeholder="username"
+            {...register("username", { required: "Username is required" })}
+            error={!!errors.username}
+            helperText={errors.username && errors.username.message}
+            sx={{ input: { color: 'white' } }}
           />
         </div>
-        <div>
-          <label className={styles.label} htmlFor="password1">
-            Password{" "}
-          </label>
-          <input
-            className={styles.field}
-            placeholder="password" {...register("password1", { required: true })}
+        <div style={{ marginBottom: "16px" }}>
+          <TextField
+            fullWidth
+            label={"Password"}
+            placeholder="password"
             type="password"
+            {...register("password1", { required: "Password is required" })}
+            error={!!errors.password1}
+            helperText={errors.password1 && errors.password1.message}
+            sx={{ input: { color: 'white' } }}
           />
         </div>
         <div>
-          <label className={styles.label} htmlFor="password2">
-            Confirm Password{" "}
-          </label>
-          <input
-            className={styles.field}
+          <TextField
+            fullWidth
             type="password"
             placeholder="confirm password"
             {...register("password2", {
-              required: true,
+              required: "Password confirmation is required",
               validate: (val) => {
-                if (watch('password1') != val) {
-                  return "Passwords didn't match"
-                  console.log("Passwords didn't match")
+                if (watch("password1") !== val) {
+                  return "Passwords don't match";
                 }
               },
-             })}
-          />
-          <ErrorMessage errors={errors} name="password2" />
-
-          <ErrorMessage
-            errors={errors}
-            name="password1"
-            render={({ message }) => <p>{message}</p>}
+            })}
+            error={!!errors.password2}
+            helperText={errors.password2 && errors.password2.message}
+            sx={{ input: { color: 'white' } }}
           />
         </div>
       </div>
       <div>
-        <button className={styles.btn} type="submit">
+        <Button className={styles.btn} type="submit" variant="contained" color="primary">
           Create Account
-        </button>
+        </Button>
       </div>
       <span className={styles.span}>
         Already have an account?{" "}
